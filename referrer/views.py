aff227346f3b1d36573396a28646f050942082ea
Ahmed Hassan <ah.hassan@nu.edu.eg>
2022-08-16 04:23:47 +0200

    add pagination and search

--- referrer/views.py
+++ referrer/views.py
@@ -3,12 +3,22 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import DeleteView, CreateView, UpdateView
 from .models import Referrer
 from django.urls import reverse_lazy
+from django.db.models import Q
+from .models import Referrer
 # Create your views here.
 
 class ReferrerListView(ListView):
     model = Referrer
     template_name = 'referrers.html'
-
+    paginate_by = 10
+    def get_queryset(self):
+        queryset = Referrer.objects.all()
+        # Search
+        search_value = self.request.GET.get('search_value',default="")
+        admin_student_list1 = Q(
+            referrerName__contains=search_value)
+        queryset = Referrer.objects.filter(admin_student_list1)
+        return queryset
 
 class ReferrerCreateView(CreateView):
     model = Referrer
