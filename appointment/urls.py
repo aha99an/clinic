0ccfd464a5b0862a179aeab5fd71cb9e25a77f8b
ahmedlhassan <a.ha99an@gmail.com>
2022-12-28 03:49:55 +0200

    test upload

--- appointment/urls.py
+++ appointment/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AppointmentCreateView, AppointmentDeleteView, AppointmentListView, AppointmentUpdateView, AppointmentsPatientDetailView, import_csvappo
+from .views import AppointmentCreateView, AppointmentDeleteView, AppointmentListView, AppointmentUpdateView, AppointmentsPatientDetailView, import_csvappo, import_csvAppoForOperation
 
 urlpatterns = [
 path('', AppointmentListView.as_view(), name='appointments'),
@@ -8,6 +8,10 @@ path('appointment/<int:pk>/edit/', AppointmentUpdateView.as_view(), name='appoin
 path('appointment/<int:pk>/delete/',  AppointmentDeleteView.as_view(), name='appointment_delete'),
 path('patient/<int:pk>/appointmentspatient/',  AppointmentsPatientDetailView.as_view(), name='appointments_for_Patient'),
 path('import_csvappo',  import_csvappo, name='import-csvappo'),
+path('import_csvAppoForOperation',  import_csvAppoForOperation, name='import-csvAppoForOperation'),
+
+
+
 
 
 
--- csv_files/edit_appointment_file.py
+++ csv_files/edit_appointment_file.py
@@ -5,28 +5,20 @@ def edit():
     name = []
     date = []
     print ('111111111111111111111111111111')
-    with open('/home/ahmed/Desktop/Clinic_project/clinic/csv_files/last_update/appointment-2022-10-20.csv', 'r+', encoding='utf-16-le') as file:
+    with open('/home/ahmed/Desktop/Clinic_project/clinic/csv_files/last_update/export-2022-12-24_18_25_1111111111111111.csv', 'r+', encoding='utf-16-le') as file:
         reader = csv.reader(file)
         writer = csv.writer(file)
         dateAfterEdit= []
         rows = []
         for row in reader: 
-            # print (row)
-            # name =row[0]
-            # print (name)
             date  = row[1]
-            # print (date)
-            # satt = row[2]
             date = str(date)
             d= date[0:2]
             m = date[3:5]
             y= date[6:10]
             date = y + '-'+ m  +  '-'  + d
             row[1]= date 
-            # print (d)
-            row[4]= "1000-01-01"
             rows.append(row)
-            # print (date)
             print (row)
         writer.writerow(['111111111111111111111111111111111111','111111111111111111111111111111111111','111111111111111111111111111111111111','111111111111111111111111111111111111'])
 
