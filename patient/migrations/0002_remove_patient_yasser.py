7d408ecf6629ac6d52e5518b55a5f48531643de6
ahmedlhassan <a.ha99an@gmail.com>
2022-12-29 17:10:49 +0200

    edit

--- patient/migrations/0002_remove_patient_yasser.py
+++ patient/migrations/0002_remove_patient_yasser.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='patient',
-            name='yasser',
-        ),
+        # migrations.RemoveField(
+        #     model_name='patient',
+        #     name='yasser',
+        # ),
     ]
