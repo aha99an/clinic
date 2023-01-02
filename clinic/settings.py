6ed8d67b5860d6ebc4b70135b25c196045c759c6
ahmedlhassan <a.ha99an@gmail.com>
2023-01-02 02:11:27 +0200

    fix: ignore .env and load env variables

--- clinic/settings.py
+++ clinic/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
+import environ
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
